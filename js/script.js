4a59ce4f35b7b78417ff0e585bde3f5e70b6e94e
Valeria Curseri <curserivaleria@gmail.com>
2020-09-02 11:16:29 +0200

    File di base e html completo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Milestone 1:
+// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
+// Titolo
+// Titolo Originale
+// Lingua
+// Voto
+
+$(document).ready(function(){
+
+})
