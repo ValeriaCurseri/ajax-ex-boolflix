33a8f30e2abe37a249b4ad52884cd127a6935916
Valeria Curseri <curserivaleria@gmail.com>
2020-09-03 11:45:34 +0200

    Commenti per voto in stelle

--- js/script.js
+++ js/script.js
@@ -18,10 +18,18 @@ $(document).ready(function(){
 
 })
 
-// Milestone 2:
+// VOTO IN STELLINE
 // Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
 // Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
+// 1- divido il voto per due
+// 2- arrotondo per eccesso con fz math >> ottengo num e cerco 5 - num
+// 3- ciclo for per generare tante stelle quante num
+// 4- ciclo for per generare tante stelle vuote quante 5 - num 
+
+// LINGUA IN BANDIERA
 // Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+
+// RICERCA SERIE TV
 // Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
 // Qui un esempio di chiamata per le serie tv:
 // https:api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
