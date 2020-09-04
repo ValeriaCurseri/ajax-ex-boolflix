a6b0805956586895db49f0c9ea82295afc3341e2
Valeria Curseri <curserivaleria@gmail.com>
2020-09-04 15:19:17 +0200

    Pulizia e ordine dopo correzione ma non funzionano le chiamate API

--- js/script.js
+++ js/script.js
@@ -1,92 +1,124 @@
 $(document).ready(function(){
 
+    var url1 = 'https://api.theFilmdb.org/3/search/movie';
+    var url2 = 'https://api.theFilmdb.org/3/search/tv';
+
     $('#query').keyup(function(event){
-        if(event.keyCode == 13 || event.which == 13){       // al click del tasto invio
-            var query = $('#query').val().toLowerCase();        // memorizzo la query dell'utente e la trasformo in minuscolo così che sia possibile cercare in maiuscolo
-            $('#lista').empty();                                // svuoto il div risultati
-            // var cercaFilm = ricercaFilm(query);                 // attivo la fz per la ricerca dei film
-            // var cercaSerie = ricercaSerie(query);               // attivo la fz per la ricerca delle serie tv
-            var cercaFilm = ricerca(query,'movie');
-            var cercaSerie = ricerca(query,'tv');
-            $('#query').val('');                                // pulisco il campo di input
+        if(event.keyCode == 13 || event.which == 13){   // al click del tasto invio
+            var query = $('#query').val().toLowerCase();    // memorizzo la query dell'utente e la trasformo in minuscolo così che sia possibile cercare in maiuscolo
+            cleanResults();                                 // pulisco i risultati e l'input per fare una nuova ricerca
+            ricerca(query,url1,'Film');                     // attivo la fz per la ricerca dei film
+            ricerca(query,url2,'Serie TV');                 // attivo la fz per la ricerca delle serie Serie TV
         }
     })
 
-    $('#cerca').click(function(){                           // al click sul bottone
-        var query = $('#query').val().toLowerCase();            // memorizzo la query dell'utente e la trasformo in minuscolo così che sia possibile cercare in maiuscolo
-        $('#lista').empty();                                    // svuoto il div risultati
-        // var cercaFilm = ricercaFilm(query);                 // attivo la fz per la ricerca dei film
-        // var cercaSerie = ricercaSerie(query);               // attivo la fz per la ricerca delle serie tv
-        var cercaFilm = ricerca(query,'movie');
-        var cercaSerie = ricerca(query,'tv');
-        $('#query').val('');                                    // pulisco il campo di input
+    $('#cerca').click(function(){                       // al click sul bottone
+        var query = $('#query').val().toLowerCase();        // memorizzo la query dell'utente e la trasformo in minuscolo così che sia possibile cercare in maiuscolo
+        cleanResults();                                     // pulisco i risultati e l'input per fare una nuova ricerca
+        ricerca(query,url1,'Film');                         // attivo la fz per la ricerca dei film
+        ricerca(query,url2,'Serie TV');                     // attivo la fz per la ricerca delle serie Serie TV
     })
-
 })
+
+// DA FARE
+// - stampa in due div diversi con due messaggi diversi di errore
+
+// NON FUNZIONA
+// - messaggio di non risultati
 
 // -- funzioni -- //
 
-function ricerca(data,type){
-    $.ajax({                                                            // attivo la chiamata API
-        url:'https://api.themoviedb.org/3/search/' + type,
-        method:'GET',
-        data:{                                                          // aggiungo le chiavi
-            api_key:'6cdc8707c60410cd9aef476067301b80',
-            language:'it-IT',
-            query: data
-        },
-        success: function(risposta){
-            stampa(risposta,type);
-        },
-        error: function(){
-            alert('Si è verificato un errore');
+function ricerca(data,url,type){
+    console.log(data);
+    console.log(url);
+    console.log(type);
+    $.ajax(
+        {                                       // attivo la chiamata API con l'attributo type
+            url: url,
+            method:'GET',
+            data:{                              // aggiungo le chiavi
+                api_key:'6cdc8707c60410cd9aef476067301b80',
+                language:'it-IT',
+                query: data
+            },
+            success: function(risposta){
+                console.log('ok');
+                if (data.total_results != 0){   // SE il numero di risultati è diverso da 0
+                    stampa(risposta,type);          // stampo i risultati
+                } else {                        // ALTRIMENTI: se il numero di risultati è 0
+                    noResults(type);                    // mostro messaggio
+                }
+            },
+            error: function(){
+                alert('Si è verificato un errore');
+            }
         }
-    })
+    )
 }
 
 function stampa(data,type){
-    if (data.total_results == 0){                                       // SE il numero di risultati è 0
-        $('#risultati').text('Non è stato trovato alcun risultato');    // mostro il messaggio
-    } else {                                                            // ALTRIMENTI
-        var source = $("#entry-template").html();
-        var template = Handlebars.compile(source);
-        for (var i = 0; i < data.results.length; i++){                  // ciclo tutti i risultati dell'array results
-            var voto = data.results[i].vote_average;
-            var linguaOriginale = data.results[i].original_language
-            if (type == 'movie') {                                      // SE il type corrisponde a movie
-                var titoloOriginale = data.results[i].original_title;
-                var titolo = data.results[i].title;
-                var context = {                                         // specifico context per riuscire a gestire meglio i risultati
-                    original_title: titoloOriginale,
-                    title: titolo,
-                    tipo: 'Film',
-                    original_language: simboloLingua(linguaOriginale),
-                    vote_average: stelline(voto)                        // dal voto genero le stelline
-                }
-            } else {                                                    // ALTRIMENTI: se corrisponde a tv
-                var titoloOriginale = data.results[i].original_name;
-                var titolo = data.results[i].name;
-                var context = {                                         // specifico context per riuscire a gestire meglio i risultati
-                    original_name: titoloOriginale,
-                    name: titolo,
-                    tipo: 'Serie TV',
-                    original_language: simboloLingua(linguaOriginale),
-                    vote_average: stelline(voto)                        // dal voto genero le stelline
-                }
-            }
-            var html = template(context);
-            $('#lista').append(html);                               // compilo la pagina con i valori dei risultati
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    for (var i = 0; i < data.results.length; i++){              // ciclo tutti i risultati dell'array results
+        var voto = data.results[i].vote_average;
+        var linguaOriginale = data.results[i].original_language;
+        if (type == 'Film') {                                  // SE il type corrisponde a Film
+            var titoloOriginale = data.results[i].original_title;
+            var titolo = data.results[i].title;
+            // var elementoStampa = '.risultati.Film .lista';
+        } else if (type == 'Serie TV') {                              // ALTRIMENTI: se corrisponde a Serie TV
+            var titoloOriginale = data.results[i].original_name;
+            var titolo = data.results[i].name;
+            // var elementoStampa = '.risultati.Serie TV .lista';
         };
+        var context = {                                         // specifico context per riuscire a gestire meglio i risultati
+            titoloOriginale: titoloOriginale,
+            titolo: titolo,
+            tipo: type,
+            original_language: simboloLingua(linguaOriginale),
+            vote_average: stelline(voto)                        // dal voto genero le stelline
+        }
+        var html = template(context);
+        if (type == 'Film') {                                  // SE il type corrisponde a Film
+            $('.risultati.movie .lista').append(html);                               // compilo la pagina con i valori dei risultati
+        } else if (type == 'Serie TV') {                              // ALTRIMENTI: se corrisponde a Serie TV
+            $('.risultati.tv .lista').append(html);                               // compilo la pagina con i valori dei risultati
+        }
     };
+}
+
+function noResults(type){
+    var source = $("#noresults-template").html();
+    var template = Handlebars.compile(source);
+    var context = {                                         // specifico context per riuscire a gestire meglio i risultati
+        risultato: 'Non ci sono risultati nella sezione: ' + type
+    }
+    var html = template(context);
+    if (type == 'Film') {                                  // SE il type corrisponde a Film
+        $('.risultati.movie .lista').append(html);                               // compilo la pagina con i valori dei risultati
+    } else if (type == 'Serie TV') {                              // ALTRIMENTI: se corrisponde a Serie TV
+        $('.risultati.tv .lista').append(html);                               // compilo la pagina con i valori dei risultati
+    }
+    // return $('.risultati.' + type + ' .lista').text('Non è stato trovato alcun risultato');
+    // return alert('Non è stato trovato alcun risultato tra ' + type);
+}
+
+function cleanResults(){
+    $('.lista').empty();                                // svuoto il div risultati
+    $('#query').val('');                                // pulisco il campo di input
 }
 
 function stelline(num){
     var votoDiviso = num / 2;                           // 1- divido il voto per due
-    var votoArrotondato = Math.ceil(votoDiviso);        // 2- arrotondo per eccesso con fz math >> ottengo num e cerco 5 - num
+    var votoArrotondato = Math.floor(votoDiviso);       // 2- arrotondo per eccesso con fz math >> ottengo num e cerco 5 - num
+    var resto = votoArrotondato % 2;
     var stella = '';                                    // 3- imposto stringa vuota per inserire poi le stelle di fontawesome
-    for (var i = 1; i < 6; i++){                        // 4- ciclo for per generare stelle (i da 1 a 5 per confrontarlo con votoArrotondato)
-        if (i <= votoArrotondato) {                     // 5- SE i <= votoArrotondato genero stelle intere
+    for (var i = 0; i < 5; i++){                        // 4- ciclo for per generare stelle (i da 0 a 5 per confrontarlo con votoArrotondato)
+        if (i < votoArrotondato) {                      // 5- SE i <= votoArrotondato genero stelle intere
             stella += '<i class="fas fa-star"></i>';    // 6- aggiungo nella stringa vuota il codice fontawesome per stella piena
+        } else if (resto != 0){
+            stella += '<i class="fas fa-star-half-alt"></i>';
+            resto = 0;
         } else {                                        // 7- ALTRIMENTI: SE i > votoArrotondato genero stelle vuote
             stella += '<i class="far fa-star"></i>';    // 8- aggiungo nella stringa vuota il codice fontawesome per stella vuota
         };
@@ -95,11 +127,10 @@ function stelline(num){
 };
 
 function simboloLingua(lingua){
-    var risultato = '';                                     // 1- imposto stringa vuota per inserire poi il tag delle immagini
-    if (lingua == 'it' || lingua == 'en'){                  // 2- SE la proprietà è it è en
-        risultato = '<img src="img/' + lingua + '.svg"/>';  // 3- inserisco il tag img con la variabile uguale alla proprietà
-    } else {                                                // 4- ALTRIMENTI: SE la proprietà è diversa da it e en
-        risultato = lingua;                                 // 5- ritorno solo la proprietà, come al solito
+    var language = ['it', 'en'];
+    if (language.includes(lingua)){                     // 1- SE la proprietà è it è en
+        return '<img src="img/' + lingua + '.svg"/>';   // 2- inserisco il tag img con la variabile uguale alla proprietà
+    } else {                                            // 3- ALTRIMENTI: SE la proprietà è diversa da it e en
+        return lingua;                                  // 4- ritorno solo la proprietà, come al solito
     }
-    return risultato;
 }
